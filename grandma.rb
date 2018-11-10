c0587df11743c3b909a6dc14f41e48533a04a53d
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-11-10 13:51:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,8 +18,10 @@ def speak_to_grandma(phrase)
     return 'I LOVE YOU TOO PUMPKIN!'
     
   elsif phrase == phrase.upcase && phrase != 'I LOVE YOU GRANDMA!'
-    return 
+    return 'NO, NOT SINCE 1938!'
+    
   else
     return 'HUH?! SPEAK UP, SONNY!'
   end
+  
 end
